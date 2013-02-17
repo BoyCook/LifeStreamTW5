ce09559a79090a49b43d263afb005d3193393b77
Craig Cook <boycook@me.com>
2013-02-17 14:10:40 +0000

    Improved date diff to show upper and lower time unit i.e 3years 4months

--- editions/lifestream/tiddlers/modules/widgets/datediff.js
+++ editions/lifestream/tiddlers/modules/widgets/datediff.js
@@ -12,7 +12,17 @@ Implements the version widget.
 /*global $tw: false */
 "use strict";
 
-var DateDiff = function(d1, d2) {
+var DateDiff = function (d1, d2) {
+    this.units = {
+        second: 1000,
+        minute: 60000,
+        hour: 3600000,
+        day: 86400000,
+        week: 604800000,
+        month: 2592000000,
+        year: 31536000000
+    };
+
     this.d1 = d1;
     this.d2 = d2;
     return this;
@@ -25,22 +35,33 @@ DateDiff.prototype.diff = function() {
         return "Just now";
     } else if (diff < 75000) { // < 75 seconds
         return "About a minute ago";
-    } else if (diff < 3600000) { // < 1 hour (1000*60*60)
-        return Math.round(diff / 60000) + " minutes ago";
-    } else if (diff < 86400000) { // < 1 day (1000*60*60*24)
-        return Math.round(diff / 3600000) + " hours ago";
-    } else if (diff < 604800000) { // < 1 week (1000*60*60*24*7)
-        return Math.round(diff / 86400000) + " days ago";
-    } else if (diff < 3144960000) { // < 1 year (1000*60*60*24*52)
-        return Math.round(diff / 604800000) + " weeks ago";
-    } else if (diff > 3144960000) { // > 1 year (1000*60*60*24*52)
-        return Math.round(diff / 3144960000) + " years ago"; //TODO: add in months
+    } else if (diff < this.units.hour) {
+        return Math.round(diff / this.units.minute) + " minutes ago";
+    } else if (diff < this.units.day) {
+        return this.calculate("hour", this.units.hour, "minute", this.units.minute, diff);
+    } else if (diff < this.units.week) {
+        return this.calculate("day", this.units.day, "hour", this.units.hour, diff);
+    } else if (diff < this.units.year) { // < 1 year (1000*60*60*24*52)
+        return this.calculate("week", this.units.week, "day", this.units.day, diff);
+    } else if (diff > this.units.year) { // > 1 year (1000*60*60*24*52)
+        return this.calculate("year", this.units.year, "month", this.units.month, diff);
     }
     return diff + " ms ago"
 };
 
-DateDiff.prototype.calculate = function() {
+DateDiff.prototype.calculate = function(unitName, unit, lowerUnitName, lowerUnit, diff) {
+    var upperDiff = Math.floor(diff / unit);
+    var remainder = Math.round(diff % unit);
+    var lowerDiff = Math.floor(remainder / lowerUnit);
+    if (remainder >= 1 && lowerDiff >= 1) {
+        return this.getText(upperDiff, unitName) + " and " + this.getText(lowerDiff, lowerUnitName) + " ago";
+    } else {
+        return this.getText(Math.round(diff / unit), unitName) + " ago";
+    }
+};
 
+DateDiff.prototype.getText = function(value, name) {
+    return value + " " + (value > 1 ? (name + "s") : name);
 };
 
 var DateDiffWidget = function(renderer) {
