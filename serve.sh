754145b57328fe88f8ea21559a0df1d230345a49
Craig Cook <boycook@me.com>
2013-02-18 15:51:57 +0000

    Added some help and setup tiddlers

--- serve.sh
+++ serve.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+# Serve TiddlyWiki5 over HTTP
 
-# serve TiddlyWiki5 over HTTP
-
-
+# Check for edition location
 if [  -z "$TW5_EDITION" ]; then
     TW5_EDITION=editions/lifestream
 fi
@@ -12,23 +11,31 @@ if [  ! -d "$TW5_EDITION" ]; then
     exit 1
 fi
 
+# Check for tiddlywiki.js file
+if [  -z "$TW5_LOC" ]; then
+    TW5_LOC="./node_modules/tiddlywiki/tiddlywiki.js"
+fi
+
+if [  ! -f "$TW5_LOC" ]; then
+    echo 'A valid TW5_LOC file must be set'
+    exit 1
+fi
+
 echo "Using TW5_EDITION as [$TW5_EDITION]"
-pushd $TW5_EDITION > /dev/null
+echo "Using TW5_LOC as [$TW5_LOC]"
 
 if [ $# -eq 1 ]
 then
-    nohup node ../../node_modules/tiddlywiki/tiddlywiki.js \
+    nohup node $TW5_LOC \
+        $TW5_EDITION \
         --verbose \
-        --lifestream ../../config.json twitter wordpress github jenkins \
+        --lifestream ./config.json twitter wordpress github jenkins \
         --server 30080 $:/core/templates/tiddlywiki5.template.html text/plain text/html \
         1> ../../webserver.log &
 else
-    node ../../node_modules/tiddlywiki/tiddlywiki.js \
+    node $TW5_LOC \
+        $TW5_EDITION \
         --verbose \
-        --lifestream ../../config.json twitter wordpress github jenkins \
         --server 30080 $:/core/templates/tiddlywiki5.template.html text/plain text/html \
         || exit 1
 fi
-
-popd > /dev/null
-
