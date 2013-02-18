f96bd3c4529e6ec6c9efb673d74f2aaa79630c01
Craig Cook <boycook@me.com>
2013-02-18 17:25:00 +0000

    Added git submodules helper script

---
+++ submodules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Add submodules
+
+#git submodule add git://github.com/BoyCook/TiddlyWiki5.git node_modules/tiddlywiki
+#git submodule add git://github.com/BoyCook/TwitterJSClient.git node_modules/twitter-js-client
+#git submodule add git://github.com/BoyCook/WordPressJSClient.git node_modules/wordpress-js-client
+#git submodule add git://github.com/BoyCook/GitHubJSClient.git node_modules/github-js-client
+#git submodule add git://github.com/BoyCook/JenkinsJSClient.git node_modules/jenkins-js-client
+
+#git clone --recursive git://github.com/BoyCook/CraigCook.git
+#git submodule foreach git pull origin master
